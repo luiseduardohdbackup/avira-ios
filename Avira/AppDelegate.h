83416f621feb39ddd7013b5dac36d5728f3268e8
Chinh Le <quocchinh.le@2359media.com>
2015-06-18 14:42:18 +0800

    Initial Commit

---
+++ Avira/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Avira
+//
+//  Created by Chinh Le on 6/18/15.
+//  Copyright (c) 2015 2359 Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Avira/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Avira
+//
+//  Created by Chinh Le on 6/18/15.
+//  Copyright (c) 2015 2359 Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
